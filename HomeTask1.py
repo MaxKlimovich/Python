b0a9b899a1f3e85f141f10c21a51dbaa309decf9
user <Klimovich.maks@gmail.com>
2022-09-24 00:20:03 +0300

    add HomeTask #1

---
+++ HomeTask1.py
@@ -0,0 +1,20 @@
+numday = int(input("введите день недели: "))
+
+if numday == 1:
+    print(f"{numday} It is Monday. Fack Day")
+elif numday == 2:
+    print(f"{numday} It is Tuesday. WorkDay")
+elif numday == 2:
+    print(f"{numday} It is Tuesday. WorkDay")
+elif numday == 3:
+    print(f"{numday} It is Wednesday. WorkDay")
+elif numday == 4:
+    print(f"{numday} It is Thursday. WorkDay 1")
+elif numday == 5:
+    print(f"{numday} It is Friday. BeerDay ")
+elif numday == 6:
+    print(f"{numday} It is Saturday. Weekday")
+elif (numday == 7 ):
+    print(f'{numday} It is Sunday. Day of Nastia ')
+else:
+    print(f'It is not day of the week!')
