c12e0ba434a95a7ebf475f45d0f88f5ecc5561ed
user <Klimovich.maks@gmail.com>
2022-09-25 11:28:55 +0300

    add add Home task #5 distance beetwen 2 point in spacefixed version

--- HomeTask5.py
+++ HomeTask5.py
@@ -7,13 +7,13 @@
 
 
 xA = int(input("Напишите коордионату точки xA: "))
-xB = int(input("Напишите коордионату точки xB: "))
-
 yA = int(input("Напишите коордионату точки yA: "))
+
+xB = int(input("Напишите коордионату точки xB: "))
 yB = int(input("Напишите коордионату точки yB: "))
 
 import math
 ab = float(math.sqrt(((xB - xA) ** 2) + ((yB - yA) ** 2)))
 a_float = ab
-limited_float = round(a_float, 2)
+limited_float = round(a_float, 3)
 print(limited_float)
