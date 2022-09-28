279c7fb4ae3e1e2fd94cbb677cb0916c9bcac667
user <Klimovich.maks@gmail.com>
2022-09-28 18:11:08 +0300

    add Home task first SUM real number

--- HomeTask2/HomeTask1.py
+++ HomeTask2/HomeTask1.py
@@ -5,3 +5,12 @@
 # - 0.67 -> 13
 # - 198.45 -> 27
 
+
+num = float(input('Enter the number: '))
+n = len(str(num)) -1
+number = int(num * 10 ** n)
+sum = 0
+while (number != 0):
+    sum = sum + number % 10 
+    number = number // 10
+print(sum)
