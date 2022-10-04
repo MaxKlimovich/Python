cc24c9467d89557c537616667838fa8c120d78d0
user <Klimovich.maks@gmail.com>
2022-10-04 11:04:49 +0300

    add practice 4 task 12

--- Practice4/Task12.py
+++ Practice4/Task12.py
@@ -1,1 +1,36 @@
 
+# 1. Задайте строку из набора чисел. Напишите программу,
+#    которая покажет большее и меньшее число.
+#    В качестве символа-разделителя используйте пробел.
+
+
+
+# line = input()
+# # list_1 = [int(x.strip(',.*')) for x in line.split() if x.replace("-", "").isdigit()]
+# list_1 = [x.strip(',.*') for x in line.split() if x.replace("-", "").isdigit()]
+# print(f'Min: {min(list_1, key=int)}')
+# print(f'Max: {max(list_1, key=int)}')
+
+
+
+# Function solution
+
+def check(str_list):
+    for i, num in enumerate(str_list):
+        str_list[i] = num.strip('.,;:?!')
+        if not str_list[i].replace("-", "").isdigit():
+            return []
+    return str_list
+
+
+def find_max_min(nums_str: str):
+    list_nums = nums_str.split()
+    right_list = check(list_nums)
+
+    if right_list:
+        return min(right_list, key=int), max(right_list, key=int)
+    print("The data is incorrect")
+    return []
+
+
+print(*find_max_min(input("Enter the numbers separated by a space: ")))
